67eaa784456865ff44cf6a2cd87578fe8436d66f
Xiaonan Tong <xiaonan@stanford.edu>
2014-02-19 22:25:52 +0000

    added mongodb integration

--- routes/project.js
+++ routes/project.js
@@ -3,6 +3,10 @@ var models = require('../models');
 exports.projectInfo = function(req, res) { 
   var projectID = req.params.id;
 
+	//query:
+	models.Project
+	.find ({"_id": projectID})
+	.exec(afterQuery);
   // query for the specific project and
   // call the following callback
 
@@ -15,6 +19,19 @@ exports.projectInfo = function(req, res) { 
 exports.addProject = function(req, res) {
   var form_data = req.body;
   console.log(form_data);
+  var newpost = new models.Project({
+	  "title": form_data['project_title'],
+		"date": form_data['date'],
+		"summary": form_data['summary'],
+		"image": form_data['image_url']
+  });
+  newpost.save(addCallback);
+  
+  function addCallback(err){
+	  if(err) {console.log(err); }
+	  res.send(500);
+	  res.redirect('/');
+  }
 
   // make a new Project and save it to the DB
   // YOU MUST send an OK response w/ res.send();
@@ -22,7 +39,18 @@ exports.addProject = function(req, res) {
 
 exports.deleteProject = function(req, res) {
   var projectID = req.params.id;
-
+  
+  models.Project
+  .find({"_id":projectID})
+  .remove()
+  .exec(deleteCallback);
+  
+	
+	function deleteCallback(err) {
+  		if(err) { console.log(err); }
+		res.send(500);
+		res.redirect("/");
+	}
   // find the project and remove it
   // YOU MUST send an OK response w/ res.send();
 }
